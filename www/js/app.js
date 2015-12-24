ca123bde0199f75ef146f9d60a1aa2fd73869915
Altay Aydemir <altayaydemir@gmail.com>
2015-12-24 22:30:45 +0200

    Finish Demo (Working on my iPhone)

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,46 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// call the angular module 'starter', 'ionic' and 'ngCordovaBeacon'
+angular.module('starter', ['ionic', 'ngCordovaBeacon'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+  $ionicPlatform.ready(function(){
+    if(window.cordova && window.cordova.plugins.Keyboard){
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.controller("ExampleController", function($scope, $rootScope, $ionicPlatform, $cordovaBeacon) {
+
+  // create beacon scope
+  $scope.beacons = {
+
+  };
+
+  // when ionic platform is rdy
+  $ionicPlatform.ready(function() {
+
+    // ??
+    $cordovaBeacon.requestWhenInUseAuthorization();
+    
+    // start looking for beacons in region
+    $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
+      
+      // append found uniquebeaconkeys to this variable
+      var uniqueBeaconKey;
+
+      for(var i = 0; i < pluginResult.beacons.length; i++){
+        uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
+        $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+      }
+
+      $scope.$apply();
+    
+    });
+
+    $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("estimote", "b9407f30-f5f8-466e-aff9-25556b57fe6d"));
+
+  });
+});
